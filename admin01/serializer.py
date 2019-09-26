b0cac1f3180c046f8c61be92f1c8ce68aa2ac8ed
dsq <2836531273@qq.com>
2019-09-26 20:29:42 +0800

    ser

--- admin01/serializer.py
+++ admin01/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from admin01.models import *
 
+
 class Userserializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -12,3 +13,14 @@ class UserLevelserializer(serializers.ModelSerializer):
         model = UserLevel
         fields = "__all__"
 
+
+class ConditionSerializersModel(serializers.ModelSerializer):
+    class Meta:
+        model = UserLevelCondition
+        fields = '__all__'
+
+
+class LevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserLevel
+        fields = '__all__'
