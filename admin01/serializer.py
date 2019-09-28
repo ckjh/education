eaf89074c7613fe9896c1eebf1adbf52db542275
manage <manager@163.com>
2019-09-28 16:17:59 +0800

    ok

--- admin01/serializer.py
+++ admin01/serializer.py
@@ -2,25 +2,43 @@ from rest_framework import serializers
 from admin01.models import *
 
 
-class Userserializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
-
-
-class UserLevelserializer(serializers.ModelSerializer):
+# 用户等级序列化类
+class UserLevelSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserLevel
         fields = "__all__"
 
 
+# 用户等级条件序列化类
 class ConditionSerializersModel(serializers.ModelSerializer):
+    level = serializers.CharField(source='level.level')
+    level_id = serializers.IntegerField()
+
     class Meta:
         model = UserLevelCondition
         fields = '__all__'
 
 
-class LevelSerializer(serializers.ModelSerializer):
+# 用户等级条件反序列化类
+class UserLevelConditionSerializers(serializers.Serializer):
+    level_id = serializers.IntegerField()
+    time = serializers.IntegerField()
+    amount = serializers.DecimalField(max_digits=7, decimal_places=2)
+
+    def create(self, data):
+        m = UserLevelCondition.objects.create(**data)
+        return m
+
+    def update(self, instance, validated_data):
+        instance.level_id = validated_data['level_id']
+        instance.time = validated_data['time']
+        instance.amount = validated_data['amount']
+        instance.save()
+        return instance
+
+
+# 标签的序列化类
+class TagSerializersModel(serializers.ModelSerializer):
     class Meta:
-        model = UserLevel
+        model = Tag
         fields = '__all__'
--- admin01/views.py
+++ admin01/views.py
@@ -3,87 +3,191 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 # Create your views here.
 from admin01.serializer import *
+from django.core.paginator import Paginator
 
 
-class Userlist(APIView):
-    def post(self, request):
-        id = request.POST.get('id')
+# 用户等级管理
+class LevelAPIView(APIView):
+    def post(self, request):  # 添加等级
+        ret = {}
+        try:
+            data = request.data
+            UserLevel.objects.create(level=data['level'])
+            ret['code'] = 200
+            ret['message'] = '成功'
+        except Exception as es:
+            print(es)
+            ret['code'] = 601
+            ret['message'] = '数据库错误'
+        return Response(ret)
+
+    def delete(self, request):  # 删除等级
+        ret = {}
+        try:
+            data = request.data
+            UserLevel.objects.get(id=data['id']).delete()
+            ret['code'] = 200
+            ret['message'] = '成功'
+        except Exception as es:
+            print(es)
+            ret['code'] = 601
+            ret['message'] = '数据库错误'
+        return Response(ret)
+
+    def put(self, request):  # 修改等级
+        ret = {}
+        try:
+            data = request.data
+            obj = UserLevel.objects.get(id=data['id'])
+            obj.level = data['level']
+            obj.save()
+            ret['code'] = 200
+            ret['message'] = '成功'
+        except Exception as es:
+            print(es)
+            ret['code'] = 601
+            ret['message'] = '数据库错误'
+        return Response(ret)
+
+    def get(self, request):  # 展示标签
+        ret = {}
+        levels = UserLevel.objects.all()
+        levels = UserLevelSerializer(levels, many=True)
+        ret['levels'] = levels.data
+        ret['code'] = 200
+        ret['message'] = '成功'
+        return Response(ret)
+
+
+# 课程标签管理
+class TagAPIView(APIView):
+    def post(self, request):  # 添加标签
+        ret = {}
+        try:
+            data = request.data
+            print(data)
+            Tag.objects.create(name=data['name'])
+            ret['code'] = 200
+            ret['message'] = '成功'
+        except Exception as es:
+            print(es)
+            ret['code'] = 601
+            ret['message'] = '数据库错误'
+        return Response(ret)
+
+    def delete(self, request):  # 删除标签
+        ret = {}
+        try:
+            data = request.data
+            Tag.objects.get(id=data['id']).delete()
+            ret['code'] = 200
+            ret['message'] = '成功'
+        except Exception as es:
+            print(es)
+            ret['code'] = 601
+            ret['message'] = '数据库错误'
+        return Response(ret)
+
+    def put(self, request):  # 修改标签
+        ret = {}
+        try:
+            data = request.data
+            obj = Tag.objects.get(id=data['id'])
+            obj.name = data['name']
+            obj.save()
+            ret['code'] = 200
+            ret['message'] = '成功'
+        except Exception as es:
+            print(es)
+            ret['code'] = 601
+            ret['message'] = '数据库错误'
+        return Response(ret)
+
+    def get(self, request):  # 展示标签
+        ret = {}
+        try:
+            tags = Tag.objects.all()
+            tags = TagSerializersModel(tags, many=True)
+            ret['tags'] = tags.data
+            ret['code'] = 200
+            ret['message'] = '成功'
+        except Exception as ex:
+            print(ex)
+            ret['code'] = 601
+            ret['message'] = '数据库错误'
+
+        return Response(ret)
+
+
+# 用户等级条件管理
+class ConditionAPIView(APIView):
+    def post(self, request):  # 添加用户等级条件 参数 id ,level_id ,amount, time
         mes = {}
-        if id:
-            UserLevel.objects.get(id=id).delete()
-            mes['code'] = 200
-            mes['msg'] = "删除成功"
-        else:
-            mes['code'] = 400
-            mes['msg'] = "删除失败"
+        try:
+            ser = UserLevelConditionSerializers(data=request.data)
+            if ser.is_valid():
+                ser.save()
+                mes['code'] = 200
+                mes['message'] = '成功'
+                mes['data'] = ser.data
+            else:
+                print(ser.errors)
+                mes['code'] = 601
+                mes['message'] = '失败'
+        except Exception as ex:
+            print(ex)
+            mes['code'] = 601
+            mes['message'] = '数据库错误'
+        return Response(mes)
+
+    def delete(self, request):  # 删除条件
+        ret = {}
+        try:
+            data = request.data
+            UserLevelCondition.objects.get(id=data['id']).delete()
+            ret['code'] = 200
+            ret['message'] = '成功'
+        except Exception as es:
+            print(es)
+            ret['code'] = 601
+            ret['message'] = '数据库错误'
+        return Response(ret)
+
+    def put(self, request):  # 修改条件 参数 id ,level_id ,amount, time
+        mes = {}
+        try:
+            id = request.data['id']
+            c1 = UserLevelCondition.objects.get(id=id)
+            ser = UserLevelConditionSerializers(c1, data=request.data)
+            if ser.is_valid():
+                ser.save()
+                mes['code'] = 200
+                mes['message'] = '成功'
+                mes['data'] = ser.data
+            else:
+                print(ser.errors)
+                mes['code'] = 601
+                mes['message'] = '失败'
+        except Exception as es:
+            mes['code'] = 601
+            mes['message'] = '数据库错误'
         return Response(mes)
 
     def get(self, request):
-        use = UserLevel.objects.all()
-        u = UserLevelserializer(use, many=True)
-        return Response(u.data)
-
-
-
-
-
-class ShowCondition(APIView):
-    def get(self, request):
         ret = {}
         try:
+            p = request.GET.get('p')
             conditions = UserLevelCondition.objects.all()
+            paginator = Paginator(conditions, 2)
+            conditions = paginator.page(int(p))
             conditions = ConditionSerializersModel(conditions, many=True)
             ret['conditions'] = conditions.data
+            ret['currentPage'] = p
+            ret['totalPage'] = paginator.num_pages
             ret['code'] = 200
             ret['message'] = '成功'
-        except:
+        except Exception as ex:
+            print(ex)
             ret['code'] = 601
             ret['message'] = '数据库错误'
         return Response(ret)
-
-
-class DeleteCondition(APIView):
-    def get(self, request):
-        ret = {}
-        try:
-            id = request.GET.get('id')
-            UserLevelCondition.objects.get(id=id).delete()
-            ret['code'] = 200
-            ret['message'] = '成功'
-        except:
-            ret['code'] = 601
-            ret['message'] = '数据库错误'
-        return Response(ret)
-
-
-class Level(APIView):
-    def post(self, request):
-        ser = LevelSerializer(data=request.data)
-        mes = {}
-        if ser.is_valid():
-            ser.save()
-            mes['code'] = 200
-            mes['message'] = '成功'
-            mes['data'] = ser.data
-        else:
-            print(ser.errors)
-            mes['code'] = 400
-            mes['message'] = '失败'
-        return Response(mes)
-
-    def put(self, request):  # todo 修改
-        content = request.data
-        id = int(content['id'])                           # 获取点击后的内容id
-        c1 = UserLevel.objects.get(id=id)
-        c = LevelSerializer(c1, data=content)
-
-        mes = {}
-        if c.is_valid():
-            c.save()
-            mes['code'] = 200
-            mes['message'] = 'ok'
-        else:
-            print(c.errors)
-            mes['code'] = 10020
-            mes['message'] = '失败'
-        return Response(mes)
